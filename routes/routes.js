7d79124251a6020ba1a9feb9b4f222700ca6ea39
NatanRyan <103397465+NatanRyan@users.noreply.github.com>
2022-11-06 17:07:09 -0300

    getbyid update and delete

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,8 @@ const controlador = require("../controller/TaskController.js");
 
 routes.get("/", controlador.getAllTasks);
 routes.post("/create", controlador.createTask);
-routes.get("/getById/:id", controlador.getById);
+routes.get("/getById/:id/:method", controlador.getById);
 routes.post("/updateOne/:id", controlador.updateOneTask);
+routes.get("/deleteOne/:id", controlador.deleteOneTaks);
 
 module.exports = routes;
